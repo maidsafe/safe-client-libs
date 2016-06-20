a09295d96fee181be0483362ce0bd319d0f594ad
krishna <krishnaof1988@gmail.com>
2016-06-20 16:17:02 +0530

    Fix for fetching file from DNS. If user `A` creates a file in a public directory and maps it to a service. User `B` cannot access the file using a registered client. This issue is fixed by allowing one more error case handling in dns/dns_operations/mod.rs Test cases added to verify the same.

--- src/dns/dns_operations/mod.rs
+++ src/dns/dns_operations/mod.rs
@@ -200,11 +200,11 @@ impl DnsOperations {
             Err(DnsError::CoreError(CoreError::OperationForbiddenForClient)) => (),
             Err(DnsError::NfsError(NfsError::CoreError(
                 CoreError::OperationForbiddenForClient))) => (),
+            Err(DnsError::DnsRecordNotFound) => (),
             Err(error) => return Err(error),
         };
-
         let (_, dns_record) =
-            try!(self.get_housing_structured_data_and_dns_record(long_name, data_decryption_keys));
+            try!(self.get_housing_structured_data_and_dns_record(long_name, data_decryption_keys));        
         dns_record.services
             .get(service_name)
             .map(|v| v.clone())
