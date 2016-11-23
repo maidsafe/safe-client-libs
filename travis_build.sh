0889cf04bc98eb5376c0d7883e244b2483d3f71a
ustulation <ustulation@gmail.com>
2016-11-23 14:29:44 +0000

    remove travis wait

--- travis_build.sh
+++ travis_build.sh
@@ -10,8 +10,8 @@ trap 'exit' ERR
 cd $TRAVIS_BUILD_DIR
 
 if [[ $TRAVIS_RUST_VERSION = stable ]]; then
-  cargo test --release --no-run
-  cargo test --release --features use-mock-routing
+  cargo test --release --no-run --verbose
+  cargo test --release --features use-mock-routing --verbose
 fi
 
 # Hide expanded commands while running
